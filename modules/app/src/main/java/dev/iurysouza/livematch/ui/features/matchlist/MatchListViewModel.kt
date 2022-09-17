4ef9ceff7ee13e1aebc96c8acb3a8c4c8775d85c
Iury Souza <iurysza@gmail.com>
2022-09-17 11:13:58 +0200

    cleanup MatchListViewModel

--- modules/app/src/main/java/dev/iurysouza/livematch/ui/features/matchlist/MatchListViewModel.kt
+++ modules/app/src/main/java/dev/iurysouza/livematch/ui/features/matchlist/MatchListViewModel.kt
@@ -3,17 +3,18 @@ package dev.iurysouza.livematch.ui.features.matchlist
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import arrow.core.Either.Companion.catch
 import arrow.core.continuations.either
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.iurysouza.livematch.R
 import dev.iurysouza.livematch.domain.adapters.DomainError
 import dev.iurysouza.livematch.domain.adapters.NetworkError
+import dev.iurysouza.livematch.domain.adapters.models.MatchHighlight
 import dev.iurysouza.livematch.domain.adapters.models.MatchThreadEntity
 import dev.iurysouza.livematch.domain.auth.RefreshTokenIfNeededUseCase
+import dev.iurysouza.livematch.domain.highlights.GetMatchHighlightsUseCase
 import dev.iurysouza.livematch.domain.matchlist.FetchLatestMatchThreadsForTodayUseCase
-import dev.iurysouza.livematch.ui.features.matchthread.InvalidMatchId
-import dev.iurysouza.livematch.ui.features.matchthread.MatchThread
+import dev.iurysouza.livematch.ui.features.matchthread.MatchThreadMapper
+import dev.iurysouza.livematch.ui.features.matchthread.toMatchItem
 import dev.iurysouza.livematch.util.ResourceProvider
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -25,8 +26,10 @@ import kotlinx.coroutines.launch
 @HiltViewModel
 class MatchListViewModel @Inject constructor(
     private val resourceProvider: ResourceProvider,
-    private val fetchLatestMatchThreadsForTodayUseCase: FetchLatestMatchThreadsForTodayUseCase,
+    private val matchThreadMapper: MatchThreadMapper,
+    private val getMatchHighlights: GetMatchHighlightsUseCase,
     private val refreshTokenIfNeeded: RefreshTokenIfNeededUseCase,
+    private val fetchLatestMatchThreadsForTodayUseCase: FetchLatestMatchThreadsForTodayUseCase,
 ) : ViewModel() {
 
     val events = MutableSharedFlow<MatchListEvents>()
@@ -34,17 +37,23 @@ class MatchListViewModel @Inject constructor(
     private val _state = MutableStateFlow<MatchListState>(MatchListState.Loading)
     val state: StateFlow<MatchListState> = _state.asStateFlow()
 
+    private var lastHighlights = emptyList<MatchHighlight>()
     private var lastMatches = emptyList<MatchThreadEntity>()
 
     fun getMachList() = viewModelScope.launch {
         either {
             refreshTokenIfNeeded().bind()
-            fetchLatestMatchThreadsForTodayUseCase().bind()
+            Pair(
+                getMatchHighlights().bind(),
+                fetchLatestMatchThreadsForTodayUseCase().bind()
+            )
         }.mapLeft { error ->
             lastMatches = emptyList()
+            lastHighlights = emptyList()
             error.toErrorMsg()
-        }.map { matchList ->
+        }.map { (highlights, matchList) ->
             lastMatches = matchList
+            lastHighlights = highlights
             matchList.toMatchItem()
         }.fold(
             { _state.emit(MatchListState.Error(it)) },
@@ -53,39 +62,23 @@ class MatchListViewModel @Inject constructor(
     }
 
     fun navigateTo(matchItem: MatchItem) = viewModelScope.launch {
-        catch {
-            val matchEntity = lastMatches.find { it.id == matchItem.id }!!
-            matchEntity to matchItem
-        }.mapLeft {
-            InvalidMatchId
-        }.map { (matchEntity, matchItem) ->
-            MatchThread(
-                id = matchEntity.id,
-                title = matchItem.title,
-                competition = matchItem.competition,
-                contentByteArray = matchEntity.content.toByteArray(),
-                startTime = matchEntity.createdAt,
-            )
-        }.fold(
+        matchThreadMapper.toMatchThread(
+            matchItem = matchItem,
+            highlights = lastHighlights,
+            lastMatches = lastMatches
+        ).fold(
             { events.emit(MatchListEvents.NavigationError(it)) },
             { events.emit(MatchListEvents.NavigateToMatchThread(it)) }
         )
     }
 
-    private fun List<MatchThreadEntity>.toMatchItem(): List<MatchItem> = mapNotNull { match ->
-        runCatching {
-            val (title, subtitle) = match.title
-                .replace("Match Thread:", "")
-                .split("|")
-            MatchItem(match.id, title, subtitle)
+    private fun DomainError.toErrorMsg(): String {
+        return when (this) {
+            is NetworkError -> {
+                Log.e("MatchListViewModel", this.message.toString())
+                resourceProvider.getString(R.string.match_screen_error_no_internet)
+            }
+            else -> resourceProvider.getString(R.string.match_screen_error_default)
         }
-            .onFailure { Log.e("LiveMatch", "Error parsing match thread", it) }
-            .getOrNull()
-    }.sortedBy { it.competition }
-
-
-    private fun DomainError.toErrorMsg(): String = when (this) {
-        is NetworkError -> resourceProvider.getString(R.string.match_screen_error_no_internet)
-        else -> resourceProvider.getString(R.string.match_screen_error_default)
     }
 }
