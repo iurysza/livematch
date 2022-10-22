7d62f988b0d50393e689ae3ddca896c41ad3579c
Iury Souza <iurysza@gmail.com>
2022-10-22 14:17:53 +0200

    add collapsible highlight section

--- modules/app/src/main/java/dev/iurysouza/livematch/ui/features/matchthread/components/MatchThreadHeader.kt
+++ modules/app/src/main/java/dev/iurysouza/livematch/ui/features/matchthread/components/MatchThreadHeader.kt
@@ -1,5 +1,6 @@
 package dev.iurysouza.livematch.ui.features.matchthread.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,6 +12,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -18,6 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.halilibo.richtext.markdown.Markdown
 import com.halilibo.richtext.ui.RichText
+import dev.iurysouza.livematch.ui.components.AnimatedCellExpansion
 import dev.iurysouza.livematch.ui.features.matchthread.MediaItem
 
 @Composable
@@ -26,6 +32,7 @@ fun MatchDescription(
     description: String,
     mediaList: List<MediaItem>,
 ) {
+    var showContent by remember { mutableStateOf(false) }
     Box(modifier =
     modifier
         .wrapContentHeight()
@@ -34,9 +41,7 @@ fun MatchDescription(
         LazyColumn(
             contentPadding = PaddingValues(
                 horizontal = 12.dp,
-                vertical = 8.dp
             ),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
             content = {
                 item(description) {
                     RichText() {
@@ -46,18 +51,25 @@ fun MatchDescription(
                 if (mediaList.isNotEmpty()) {
                     item("Media") {
                         Text(
-                            text = "Highlights:",
+                            text = "Highlights",
                             fontSize = 18.sp,
                             fontWeight = FontWeight.Bold,
                             textAlign = TextAlign.Left,
                             modifier = modifier
                                 .fillMaxWidth()
+                                .clickable { showContent = !showContent }
                         )
                     }
                     items(mediaList) { item ->
-                        ItemLink(
-                            title = item.title,
-                            linkUrl = item.url
+                        AnimatedCellExpansion(
+                            modifier,
+                            showContentIf = { showContent },
+                            content = {
+                                ItemLink(
+                                    title = item.title,
+                                    linkUrl = item.url
+                                )
+                            },
                         )
                     }
                 }
