b55fa2dcdcca8f29ec49efde5fbcf09d7a1d8a8c
Iury Souza <iurysza@gmail.com>
2022-12-04 22:52:02 +0100

    fix version name

--- build-logic/android-application/src/main/kotlin/dev.iurysouza.livematch.android-application.gradle.kts
+++ build-logic/android-application/src/main/kotlin/dev.iurysouza.livematch.android-application.gradle.kts
@@ -1,3 +1,5 @@
+import dev.iurysouza.livematch.build.withKotlinOptions
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -17,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -25,22 +27,7 @@ android {
         }
     }
 
-    kotlinOptions {
-        // Treat all Kotlin warnings as errors (disabled by default)
-        allWarningsAsErrors = properties["warningsAsErrors"] as? Boolean ?: false
-
-        freeCompilerArgs = freeCompilerArgs + listOf(
-            "-opt-in=kotlin.RequiresOptIn",
-            // Enable experimental coroutines APIs, including Flow
-            "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-            "-opt-in=kotlinx.coroutines.FlowPreview",
-            "-opt-in=kotlin.Experimental",
-            // Enable experimental compose APIs
-            "-opt-in=androidx.compose.material.ExperimentalMaterialApi",
-            "-opt-in=androidx.compose.animation.ExperimentalAnimationApi",
-            "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi",
-        )
-    }
+    withKotlinOptions(this, properties)
     buildFeatures {
         compose = true
     }
