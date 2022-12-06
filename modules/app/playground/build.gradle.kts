f56ed0d00276b0188a60f51d14ce0b0901c4013d
Iury Souza <iurysza@gmail.com>
2022-12-06 13:02:12 +0100

    add playground app

---
+++ modules/app/playground/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("dev.iurysouza.livematch.android-application")
+}
+
+android {
+    namespace = "dev.iurysouza.livematch.playground"
+    buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":features:match-thread"))
+    implementation(project(":features:match-list"))
+    implementation(project(":core:footballdata"))
+    implementation(project(":core:reddit"))
+    implementation(project(":core:design-system"))
+    implementation(project(":core:common"))
+
+    implementation(platform("androidx.compose:compose-bom:2022.11.00"))
+    implementation("androidx.compose.material:material")
+    implementation(libs.bundles.composeMinimal)
+    implementation(libs.androidx.appcompat)
+
+    debugImplementation(libs.androidx.compose.ui.uiTooling)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.google.dagger.hilt.android)
+    kapt(libs.google.dagger.hilt.androidCompiler)
+    implementation(libs.google.acompanist.navigation.animation)
+    implementation(libs.bundles.arrow)
+
+    implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation("io.coil-kt:coil-svg:2.2.2")
+}
