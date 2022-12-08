66712b2f86d80b9d5471465fa4a10e52fcd65e5e
Iury Souza <iurysza@gmail.com>
2022-12-08 15:28:39 +0100

    add docs to BaseViewModel

--- modules/core/common/src/main/java/dev/iurysouza/livematch/common/storage/BaseViewModel.kt
+++ modules/core/common/src/main/java/dev/iurysouza/livematch/common/storage/BaseViewModel.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
@@ -20,21 +21,42 @@ abstract class BaseViewModel<
     abstract fun setInitialState(): UiState
 
     private val _viewState: MutableState<UiState> = mutableStateOf(initialState)
+
+    /**
+     * A [State] object that represents the current state of the ViewModel. This can be observed
+     * by collecting the `viewState` flow.
+     */
     val viewState: State<UiState> = _viewState
 
     private val _event: MutableSharedFlow<Event> = MutableSharedFlow()
 
     private val _effect: MutableSharedFlow<Effect> = MutableSharedFlow()
+
+    /**
+     * A [SharedFlow] that emits the side effects of the ViewModel. This can be observed by
+     * collecting the `effect` flow.
+     */
     val effect = _effect.asSharedFlow()
 
     init {
         subscribeToEvents()
     }
 
+    /**
+     * Sets the event that is emitted by the UI. This should be called by the UI whenever a new
+     * event occurs that should be handled by the ViewModel.
+     *
+     * @param event the event that is emitted by the UI
+     */
     fun setEvent(event: Event) {
         viewModelScope.launch { _event.emit(event) }
     }
 
+    /**
+     * Updates the current view state using the provided reducer function.
+     *
+     * @param reducer a function that takes the current view state and returns a new view state
+     */
     protected fun setState(reducer: UiState.() -> UiState) {
         val newState = viewState.value.reducer()
         _viewState.value = newState
