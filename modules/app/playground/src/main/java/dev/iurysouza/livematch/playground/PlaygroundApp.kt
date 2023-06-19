3a6297b12843e1ef7187dccabc19b334379b96e5
Iury Souza <iurysza@gmail.com>
2023-06-19 23:08:23 +0200

    Clean up playground deps

--- modules/app/playground/src/main/java/dev/iurysouza/livematch/playground/PlaygroundApp.kt
+++ modules/app/playground/src/main/java/dev/iurysouza/livematch/playground/PlaygroundApp.kt
@@ -1,25 +1,19 @@
 package dev.iurysouza.livematch.playground
 
 import android.app.Application
-import android.content.Context
 import coil.Coil
 import coil.ImageLoader
-import coil.ImageLoaderFactory
 import coil.decode.SvgDecoder
-import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
-@HiltAndroidApp
 class PlaygroundApp : Application() {
   override fun onCreate() {
     super.onCreate()
     Timber.plant(Timber.DebugTree())
-    Coil.setImageLoader(LiveMatchImageLoader(this))
-  }
-
-  private class LiveMatchImageLoader(private val context: Context) : ImageLoaderFactory {
-    override fun newImageLoader() = ImageLoader.Builder(context)
-      .components { add(SvgDecoder.Factory()) }
-      .build()
+    Coil.setImageLoader {
+      ImageLoader.Builder(this@PlaygroundApp)
+        .components { add(SvgDecoder.Factory()) }
+        .build()
+    }
   }
 }
