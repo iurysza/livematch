3f14f52491a311c9d1670b814488b64df09f96a6
Iury Souza <iurysza@gmail.com>
2023-06-19 23:03:25 +0200

    Remove uneeded dependencies

--- modules/app/main/build.gradle.kts
+++ modules/app/main/build.gradle.kts
@@ -37,23 +37,16 @@ android {
 dependencies {
   implementation(project(":features:match-thread"))
   implementation(project(":features:match-day"))
-  implementation(project(":core:footballdata"))
-  implementation(project(":core:reddit"))
   implementation(project(":core:design-system"))
   implementation(project(":core:common"))
 
   implementation(platform("androidx.compose:compose-bom:2023.04.01"))
   implementation(libs.bundles.composeMinimal)
-  implementation(libs.androidx.compose.material)
   implementation(libs.androidx.appcompat)
-
   debugImplementation(libs.androidx.compose.ui.uiTooling)
-  implementation(libs.androidx.navigation.compose)
-  implementation(libs.androidx.hilt.navigation.compose)
+
   implementation(libs.google.dagger.hilt.android)
   kapt(libs.google.dagger.hilt.androidCompiler)
-  implementation(libs.google.acompanist.navigation.animation)
-  implementation(libs.bundles.arrow)
 
   implementation(libs.coil.compose)
   implementation(libs.coil.svg)
