c5a26b1fcc3557928dea9d0bfacc75f8e7040015
Iury Souza <iurysza@gmail.com>
2023-07-01 16:26:48 +0200

    Implement MatchDayGroupedByLeague

--- modules/features/match-day/src/main/java/dev/iurysouza/livematch/matchday/Components.kt
+++ modules/features/match-day/src/main/java/dev/iurysouza/livematch/matchday/Components.kt
@@ -1,14 +1,18 @@
 package dev.iurysouza.livematch.matchday
 
+import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -16,13 +20,69 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import dev.iurysouza.livematch.matchday.models.Competition
+import dev.iurysouza.livematch.matchday.models.Fakes
 import dev.iurysouza.livematch.matchday.models.MatchUiModel
 import dev.iurysouza.livematch.matchday.models.Team
+
+@Preview(
+  uiMode = Configuration.UI_MODE_NIGHT_NO,
+)
+@Composable
+internal fun MatchDayGroupedByLeague(
+  modifier: Modifier = Modifier,
+  matchItemList: List<MatchUiModel> = Fakes.generateMatchList(10),
+  onTapMatchItem: (MatchUiModel) -> Unit = {},
+) {
+  LazyColumn(
+    modifier = modifier
+      .fillMaxHeight()
+      .padding(horizontal = 16.dp),
+  ) {
+    matchItemList.groupBy { it.competition }.forEach { (competition, matchItemList) ->
+      stickyHeader {
+        LeagueDivider(modifier, competition)
+      }
+      itemsIndexed(matchItemList) { _, matchItem ->
+        Column(
+          modifier
+            .clickable { onTapMatchItem(matchItem) }
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+            .fillMaxWidth(),
+        ) {
+          MatchItem(modifier, matchItem)
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun LeagueDivider(modifier: Modifier, competition: Competition) {
+  Row(modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+    AsyncImage(
+      modifier = modifier
+        .clip(CircleShape)
+        .size(32.dp),
+      model = competition.emblemUrl,
+      contentDescription = "${competition.name} emblem",
+    )
+    Text(
+      text = competition.name,
+      modifier = Modifier
+        .wrapContentWidth(Alignment.Start)
+        .padding(horizontal = 8.dp, vertical = 16.dp),
+      style = MaterialTheme.typography.h6.copy(color = Color(0xFF7B7B8A)),
+    )
+  }
+}
 
 @Composable
 internal fun MatchesList(
@@ -49,34 +109,35 @@ internal fun MatchItem(modifier: Modifier, match: MatchUiModel) {
   Row {
     MatchTime(
       modifier = modifier.weight(.15f),
-      match = match,
+      startTime = match.startTime,
+      elapsedMinutes = match.elapsedMinutes,
     )
     Column(
       modifier.weight(.85f),
       verticalArrangement = Arrangement.Center,
 
-    ) {
-      Team(modifier, match.homeTeam)
-      Team(modifier, match.awayTeam)
+      ) {
+      Team(modifier, match.homeTeam, match.homeTeam.name)
+      Team(modifier, match.awayTeam, match.awayTeam.name)
     }
   }
 }
 
 @Composable
-fun MatchTime(modifier: Modifier, match: MatchUiModel) {
+fun MatchTime(modifier: Modifier, startTime: String, elapsedMinutes: String) {
   Column(
     modifier,
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
     Text(
-      text = match.startTime,
+      text = startTime,
       textAlign = TextAlign.Center,
       color = MaterialTheme.colors.onPrimary,
     )
     Text(
-      text = match.elapsedMinutes,
+      text = elapsedMinutes,
       textAlign = TextAlign.Center,
-      color = if (match.elapsedMinutes.contains("'")) {
+      color = if (elapsedMinutes.contains("'")) {
         MaterialTheme.colors.primary
       } else {
         MaterialTheme.colors.onPrimary
@@ -86,7 +147,7 @@ fun MatchTime(modifier: Modifier, match: MatchUiModel) {
 }
 
 @Composable
-internal fun Team(modifier: Modifier, team: Team) {
+internal fun Team(modifier: Modifier, team: Team, homeTeamName: String) {
   Row(
     verticalAlignment = Alignment.CenterVertically,
     modifier = modifier,
@@ -97,7 +158,7 @@ internal fun Team(modifier: Modifier, team: Team) {
         .clip(RoundedCornerShape(10.dp))
         .padding(4.dp),
       model = team.crestUrl,
-      contentDescription = "${team.name} crest",
+      contentDescription = "$homeTeamName crest",
     )
     val style = if (team.isAhead) {
       TextStyle(
@@ -113,7 +174,7 @@ internal fun Team(modifier: Modifier, team: Team) {
       )
     }
     Text(
-      text = team.name,
+      text = homeTeamName,
       style = style,
       modifier = modifier.weight(.75f),
     )
@@ -125,3 +186,65 @@ internal fun Team(modifier: Modifier, team: Team) {
     )
   }
 }
+
+@Preview(
+  uiMode = Configuration.UI_MODE_NIGHT_NO,
+)
+@Composable
+fun OtherPreview() {
+  val match = MatchUiModel(
+    id = "1",
+    homeTeam = Team(
+      name = "Arsenal",
+      crestUrl = "https://crests.football-data.org/57.svg",
+      score = "1",
+      isAhead = true,
+      isHomeTeam = false,
+    ),
+    awayTeam = Team(
+      name = "Arsenal",
+      crestUrl = "https://crests.football-data.org/57.svg",
+      score = "1",
+      isAhead = true,
+      isHomeTeam = false,
+    ),
+    startTime = "19:00",
+    elapsedMinutes = "45'",
+    competition = Competition(
+      name = "Premier League",
+      emblemUrl = "https://crests.football-data.org/57.svg",
+      id = 1,
+    ),
+  )
+  MatchItem(
+    modifier = Modifier,
+    match = match,
+  )
+}
+
+@Preview(
+  uiMode = Configuration.UI_MODE_NIGHT_NO,
+)
+@Composable
+fun TeamPreview() {
+  val team = Team(
+    name = "Arsenal",
+    crestUrl = "https://crests.football-data.org/57.svg",
+    score = "1",
+    isAhead = true,
+    isHomeTeam = false,
+  )
+  Team(
+    modifier = Modifier,
+    team = Team(
+      name = "Arsenal",
+      crestUrl = "https://crests.football-data.org/57.svg",
+      score = "1",
+      isAhead = true,
+      isHomeTeam = false,
+    ),
+    homeTeamName = team.name,
+  )
+}
+
+
