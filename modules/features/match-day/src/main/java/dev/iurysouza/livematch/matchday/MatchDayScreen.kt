5bf897f11723c08a7cab8c695470dafd9b613ef5
Iury Souza <iurysza@gmail.com>
2023-07-03 21:03:11 +0200

    Refact gradientBackground

--- modules/features/match-day/src/main/java/dev/iurysouza/livematch/matchday/MatchDayScreen.kt
+++ modules/features/match-day/src/main/java/dev/iurysouza/livematch/matchday/MatchDayScreen.kt
@@ -1,7 +1,6 @@
 package dev.iurysouza.livematch.matchday
 
 import androidx.compose.animation.Crossfade
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,14 +17,11 @@ import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.iurysouza.livematch.designsystem.components.ErrorScreen
+import dev.iurysouza.livematch.designsystem.theme.gradientBackground
 import dev.iurysouza.livematch.matchday.models.MatchDayViewState
 import dev.iurysouza.livematch.matchday.models.MatchListState
 import dev.iurysouza.livematch.matchday.models.MatchUiModel
@@ -38,10 +34,6 @@ fun MatchDayScreen(
 ) {
   val refreshState = rememberPullRefreshState(uiState.isRefreshing, onRefresh = onRefresh)
 
-  val gradientColors = listOf(
-    MaterialTheme.colors.background,
-    MaterialTheme.colors.secondaryVariant,
-  )
   Scaffold(
     modifier = Modifier
       .fillMaxHeight()
@@ -77,11 +69,11 @@ fun MatchDayScreen(
       modifier = Modifier.padding(paddingValues),
       isRefreshing = uiState.isRefreshing,
       onRefresh = onRefresh,
-      revealedComponentBackgroundColor = gradientColors.last(),
+      revealedComponentBackgroundColor = MaterialTheme.colors.secondaryVariant,
     ) {
       Box(
         Modifier
-          .background(createGradientBrush(gradientColors))
+          .gradientBackground()
           .fillMaxHeight(),
       ) {
         Crossfade(targetState = uiState.matchListState, label = "MatchListStateCrossFade") { screen ->
@@ -100,17 +92,4 @@ fun MatchDayScreen(
   }
 }
 
-fun createGradientBrush(
-  colors: List<Color>,
-  isVertical: Boolean = true,
-): Brush = Brush.linearGradient(
-  colors = colors,
-  start = Offset(0f, 0f),
-  tileMode = TileMode.Clamp,
-  end = if (isVertical) {
-    Offset(0f, Float.POSITIVE_INFINITY)
-  } else {
-    Offset(Float.POSITIVE_INFINITY, 0f)
-  },
-)
 
