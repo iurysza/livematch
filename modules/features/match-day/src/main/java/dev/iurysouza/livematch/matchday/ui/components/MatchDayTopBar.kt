f5a3bd0f9d839e9ca0c737e0973ee1c982eb038f
Iury Souza <iurysza@gmail.com>
2023-08-17 21:57:27 +0200

    Refact topbar

--- modules/features/match-day/src/main/java/dev/iurysouza/livematch/matchday/ui/components/MatchDayTopBar.kt
+++ modules/features/match-day/src/main/java/dev/iurysouza/livematch/matchday/ui/components/MatchDayTopBar.kt
@@ -1,56 +1,56 @@
 package dev.iurysouza.livematch.matchday.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.iurysouza.livematch.designsystem.theme.LiveMatchAssets
 import dev.iurysouza.livematch.designsystem.theme.LiveMatchThemePreview
-import dev.iurysouza.livematch.designsystem.theme.Space.S100
-import dev.iurysouza.livematch.designsystem.theme.Space.S200
-import dev.iurysouza.livematch.designsystem.theme.Space.S300
 import dev.iurysouza.livematch.designsystem.theme.livematchassets.Clock
 import dev.iurysouza.livematch.matchday.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MatchDayTopBar(
   modifier: Modifier = Modifier,
   isChecked: Boolean = false,
   onToggle: (Boolean) -> Unit = {},
 ) {
-  TopAppBar(
-    modifier = modifier,
-    title = {
-      Text(
-        text = stringResource(R.string.app_name),
-        color = MaterialTheme.colorScheme.onPrimary,
-        modifier = Modifier
-          .padding(start = S300)
-          .fillMaxWidth()
-          .padding(S200),
-        textAlign = TextAlign.Center,
-      )
-    },
-    colors = TopAppBarDefaults.topAppBarColors(
-      containerColor = MaterialTheme.colorScheme.background,
-    ),
-    actions = {
-      SwitcherButton(isChecked = isChecked, onToggle)
-    },
+  Row(
+    verticalAlignment = Alignment.CenterVertically,
+    modifier = modifier
+      .background(MaterialTheme.colorScheme.background)
+      .padding(horizontal = 16.dp)
+      .fillMaxWidth()
+      .height(56.dp)
   )
+  {
+    Spacer(modifier = Modifier.padding(24.dp))
+    Text(
+      text = stringResource(R.string.app_name),
+      color = MaterialTheme.colorScheme.onPrimary,
+      fontSize = 24.sp,
+      modifier = Modifier
+        .fillMaxWidth()
+        .weight(1f),
+      textAlign = TextAlign.Center,
+    )
+    SwitcherButton(isChecked = isChecked, onToggle)
+  }
 }
 
 @Preview
@@ -70,25 +70,22 @@ fun SwitcherButton(
   isChecked: Boolean,
   onToggle: (Boolean) -> Unit,
 ) {
-  Row {
-    Switch(
-      modifier = Modifier.padding(end = S100),
-      checked = isChecked,
-      onCheckedChange = { onToggle(it) },
-      colors = SwitchDefaults.colors(
-        uncheckedThumbColor = MaterialTheme.colorScheme.onPrimary,
-        uncheckedTrackColor = MaterialTheme.colorScheme.onPrimary,
+  Switch(
+    checked = isChecked,
+    onCheckedChange = { onToggle(it) },
+    colors = SwitchDefaults.colors(
+      uncheckedThumbColor = MaterialTheme.colorScheme.onPrimary,
+      uncheckedTrackColor = MaterialTheme.colorScheme.onPrimary,
 
-        checkedThumbColor = MaterialTheme.colorScheme.tertiary,
-        checkedTrackColor = MaterialTheme.colorScheme.tertiary,
-        checkedIconColor = MaterialTheme.colorScheme.primary,
-      ),
-      thumbContent = {
-        Icon(
-          imageVector = LiveMatchAssets.Clock,
-          contentDescription = "Favorite Icon",
-        )
-      },
-    )
-  }
+      checkedThumbColor = MaterialTheme.colorScheme.tertiary,
+      checkedTrackColor = MaterialTheme.colorScheme.tertiary,
+      checkedIconColor = MaterialTheme.colorScheme.primary,
+    ),
+    thumbContent = {
+      Icon(
+        imageVector = LiveMatchAssets.Clock,
+        contentDescription = "Favorite Icon",
+      )
+    },
+  )
 }
